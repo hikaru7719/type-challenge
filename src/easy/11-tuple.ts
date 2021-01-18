370548952c5ddb137273624dd97c2a4e2b0a0f36
hikaru7719 <j148016s@st.u-gakugei.ac.jp>
2021-01-19 06:02:32 +0900

    add tuple

---
+++ src/easy/11-tuple.ts
@@ -0,0 +1,12 @@
+const tuple = ["tesla", "model 3", "model X", "model Y"] as const;
+
+type TupleToObject<T extends readonly any[]> = {
+  [K in T[number]]: K;
+};
+
+const result: TupleToObject<typeof tuple> = {
+  tesla: "tesla",
+  "model 3": "model 3",
+  "model X": "model X",
+  "model Y": "model Y",
+};
